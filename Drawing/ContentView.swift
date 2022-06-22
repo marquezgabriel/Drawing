ba96b1fbe3ec746db7ae0f44db42ddd1fb77f28c
Gabriel Marquez <gabrielmarquezrivas@gmail.com>
2022-06-22 15:10:36 -0400

    Initial Commit

---
+++ Drawing/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Drawing
+//
+//  Created by Gabriel Marquez on 2022-06-22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Drawing/DrawingApp.swift
@@ -0,0 +1,17 @@
+//
+//  DrawingApp.swift
+//  Drawing
+//
+//  Created by Gabriel Marquez on 2022-06-22.
+//
+
+import SwiftUI
+
+@main
+struct DrawingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
